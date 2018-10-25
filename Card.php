c2ba38bc93d7c2cdca8db62c716a21e49fb74726
oguro <oggtetu@gmail.com>
2018-10-25 23:07:06 +0900

    テストコード追加

---
+++ Card.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: og
+ * Date: 2018/10/25
+ * Time: 21:41
+ */
+
+class Card
+{
+
+}
---
+++ CardTest.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: og
+ * Date: 2018/10/25
+ * Time: 21:46
+ */
+
+
+class CardTest extends PHPUnit_Framework_TestCase
+{
+
+}
