761e3dc6b3774b0d7c4bb77892516c87e6e32bba
oguro <oggtetu@gmail.com>
2018-10-29 23:01:07 +0900

    関数を追加

--- Card.php
+++ Card.php
@@ -8,5 +8,33 @@
 
 class Card
 {
+    private $number;
+    private $suit;
+    public function __construct($number, $suit)
+    {
+        $this->number = $number;
+        $this->suit = $suit;
+    }
+
+    public function getNumber()
+    {
+        return $this->number;
+    }
+
+    public function getSuit()
+    {
+        return $this->suit;
+    }
+
+    /**
+     * @param Card $card
+     * @return bool
+     * @assert (new Card(3, 'h'), new Card(3, 's')) == true
+     * @assert (new Card(4, 'h'), new Card(3, 's')) == false
+     */
+    public function isInMatchingSet(Card $card)
+    {
+        return ($this->getNumber() == $card->getNumber());
+    }
 
 }
